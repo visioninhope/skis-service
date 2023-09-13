0114ad9d1db772169fff1e5a01ecc47a0fb326d9
EVOFORGE\dimay <dimay@evoforge.org>
2023-09-13 16:07:48 -0400

    #2404: added support for downloading files from S3

--- service/src/main/java/skills/aws/AWSSecretsManagerBeanPostProcessor.groovy
+++ service/src/main/java/skills/aws/AWSSecretsManagerBeanPostProcessor.groovy
@@ -20,36 +20,51 @@ import org.apache.commons.io.FileUtils
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.config.BeanPostProcessor
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.core.env.ConfigurableEnvironment
 import org.springframework.stereotype.Component
+import org.springframework.util.StreamUtils
+import software.amazon.awssdk.core.ResponseInputStream
 import software.amazon.awssdk.core.SdkBytes
+import software.amazon.awssdk.services.s3.S3Client
+import software.amazon.awssdk.services.s3.model.GetObjectResponse
 import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient
 import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest
 import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueResponse
 
 @Slf4j
 @Component
-@ConfigurationProperties("skills.aws.secrets-manager.binary")
+@ConfigurationProperties("skills.aws")
 class AWSSecretsManagerBeanPostProcessor implements BeanPostProcessor {
 
+    @Autowired(required = false)
     SecretsManagerClient secretsManagerClient;
+
+    @Autowired(required = false)
+    S3Client s3Client
 
     // injected by @ConfigurationProperties
     List<String> downloadToFiles
+
+    private boolean firstRun = true
 
     private static class SecretFile {
         String secretName
         File file
     }
 
+    private static class S3File {
+        String s3BucketName
+        String s3FileName
+        File file
+    }
+
     private List<SecretFile> loadSecretFileFromProps() {
-        downloadToFiles.collect {
-            String[] split = it.split("=>")
+        downloadToFiles.findAll({ it.startsWith("secret:") }) collect {
+            String[] split = it.substring(7).split("=>")
             String secretName = split[0]
             String fileLocation = split[1]
 
             if (!secretName || !fileLocation) {
-                throw new IllegalArgumentException("File [skills.aws.secretsManager.binarySecret.downloadToFile] property must follow <secret-name>=><file-location> format")
+                throw new IllegalArgumentException("File [skills.aws.downloadToFile] property must follow secret:<secret-name>=><file-location> format")
             }
             File fileDest = new File(fileLocation)
             if (fileDest.exists()) {
@@ -59,16 +74,53 @@ class AWSSecretsManagerBeanPostProcessor implements BeanPostProcessor {
         }
     }
 
+    private List<S3File> loadS3FilesFromProps() {
+        downloadToFiles.findAll({ it.startsWith("s3:") }) collect {
+            String[] split = it.substring(3).split("=>")
+            String s3Location = split[0]
+            String fileLocation = split[1]
+
+            String expectedFormat = "File [skills.aws.downloadToFile] property must follow s3:<bucket-name>/<file-name>=><file-location> format instead was"
+            if (!s3Location || !fileLocation) {
+                throw new IllegalArgumentException("${expectedFormat} [${it}]")
+            }
+            String[] s3LocSplit = s3Location.split("/")
+            String s3BucketName = s3LocSplit[0]
+            String s3FileName = s3LocSplit[1]
+            if (!s3BucketName || !s3FileName) {
+                throw new IllegalArgumentException("${expectedFormat} [${it}]")
+            }
+            File fileDest = new File(fileLocation)
+            if (fileDest.exists()) {
+                throw new IllegalArgumentException("File [${fileDest.absolutePath}] already exists from [${it}] property")
+            }
+            return new S3File(s3BucketName: s3BucketName, s3FileName: s3FileName, file: fileDest)
+        }
+    }
+
     @Override
     Object postProcessBeforeInitialization(Object bean, String beanName) {
+
         // Code to be executed after properties are loaded but before beans are initialized
-        if (bean instanceof ConfigurableEnvironment && downloadToFiles) {
-            loadSecretFileFromProps().each {
+        if (firstRun && downloadToFiles) {
+            firstRun = false
+            List<SecretFile> secretFiles = loadSecretFileFromProps()
+            List<S3File> s3Files = loadS3FilesFromProps()
+
+            secretFiles.each {
                 log.info("Downloading secret [{}] to [{}]", it.secretName, it.file.absolutePath)
                 GetSecretValueRequest request = GetSecretValueRequest.builder().secretId(it.secretName).build()
                 GetSecretValueResponse secretValueResponse = secretsManagerClient.getSecretValue(request)
                 SdkBytes sdkBytes = secretValueResponse.secretBinary()
                 FileUtils.writeByteArrayToFile(it.file, sdkBytes.asByteArray())
+            }
+
+            s3Files.each {
+                log.info("Downloading s3 file [{}] to [{}]", "${it.s3BucketName}/${it.s3FileName}", it.file.absolutePath)
+                ResponseInputStream<GetObjectResponse> response = s3Client.getObject(
+                        request -> request.bucket(it.s3BucketName).key(it.s3FileName));
+                byte [] bytes = StreamUtils.copyToByteArray (response)
+                FileUtils.writeByteArrayToFile(it.file, bytes)
             }
         }
         return bean
